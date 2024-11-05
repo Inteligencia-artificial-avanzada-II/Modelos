3c281bda49d7e05bf008b763688b22d86686700b
JDRP010903 <danireaprado@hotmail.com>
2024-11-05 00:54:19 -0600

    Correccion

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from pymongo import MongoClient
 from config import Config
 from database.mongoConnection import getDatabase
-from controllers.mongoPredictionsController import mongoPredictionsBluePrint
+from controllers.mongoPredictionsController import mongoPredictionsBp
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,7 +11,7 @@ app.config.from_object(Config)
 mongo_client = MongoClient(app.config['MONGO_URI'])
 db = getDatabase()
 
-app.register_blueprint(mongoPredictionsBluePrint, url_prefix="/predictions")
+app.register_blueprint(mongoPredictionsBp, url_prefix="/predictions")
 
 if __name__ == "__main__":
     app.run(host=app.config['FLASK_HOST'], port=int(app.config['FLASK_PORT']), debug=bool(app.config['FLASK_DEBUG']))
