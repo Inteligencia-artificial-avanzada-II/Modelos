7da01ca37861013d20ab434ccfded040cc992a5b
JDRP010903 <danireaprado@hotmail.com>
2024-11-09 18:09:56 -0600

    Cors APlicadas

--- controllers/mongoPredictionsController.py
+++ controllers/mongoPredictionsController.py
@@ -13,11 +13,20 @@ mongoPredictionsBp = Blueprint("mongoPredictionsBp", __name__)
 prediction_service = PredictionService()
 
 
+@mongoPredictionsBp.route("/test", methods=['GET'])
+def testConnection():
+    return Response(response=json.dumps(
+        {"Message": "OK"}),
+        status=200,
+        mimetype="application/json")
+
+
 @mongoPredictionsBp.route("/predict", methods=["POST"])
 def savePrediction():
     remolques = []
     print("RUTA", f'{Config.RUTA_ARCHIVO_ORDENES}')
-    ordenes = cargar_ordenes(f'https://objectstorage.mx-queretaro-1.oraclecloud.com/p/79AJJfgFXexqbvQZWz9MJ_7qJ9xLb94V9XhEAGlRZJbwkfxL1F7gZP9EOHseYtm8/n/axnhu2vnql31/b/qrprueba/o/LibroOrdenes.csv')
+    ordenes = cargar_ordenes(
+        f'https://objectstorage.mx-queretaro-1.oraclecloud.com/p/79AJJfgFXexqbvQZWz9MJ_7qJ9xLb94V9XhEAGlRZJbwkfxL1F7gZP9EOHseYtm8/n/axnhu2vnql31/b/qrprueba/o/LibroOrdenes.csv')
     products = []
     data = request.json
     headersToken = request.headers.get('Authorization')
@@ -42,46 +51,61 @@ def savePrediction():
         apiOrdenContenedorId = f'{Config.RUTA_BACK}/orden/consultarQrUrl'
         apiFosasGetAll = f'{Config.RUTA_BACK}/fosa/consultarTodos'
         apiOrdenMongoId = f'{Config.RUTA_BACK}/ordenmongo/consultar'
-        apiPrioridadProductoAll = f'{Config.RUTA_BACK}/priorityproduct/consultarTodos'
+        apiPrioridadProductoAll = f'{
+            Config.RUTA_BACK}/priorityproduct/consultarTodos'
         responseFosas = requests.get(apiFosasGetAll)
         dataFosas = json.loads(json.dumps(responseFosas.json()))
         allFosas = dataFosas[0]['fosas']
 
         for fosa in allFosas:
             dailyFosa = fosa['daily']
-            fechaNow = '10/10/24'#datetime.now().strftime("%d/%m/%Y")
+            fechaNow = '10/10/24'  # datetime.now().strftime("%d/%m/%Y")
 
             dailyFechaData = dailyFosa.get(fechaNow)
-            contenedorIdKey = next(iter(dailyFechaData.keys())) if dailyFechaData else None
-            contenedorId = contenedorIdKey.split('-')[0] if contenedorIdKey else None
+            contenedorIdKey = next(
+                iter(dailyFechaData.keys())) if dailyFechaData else None
+            contenedorId = contenedorIdKey.split(
+                '-')[0] if contenedorIdKey else None
 
-            responseOrdenData = requests.get(f'{apiOrdenContenedorId}/{contenedorId}', headers=headersForSent) if contenedorId else None
+            responseOrdenData = requests.get(
+                f'{apiOrdenContenedorId}/{contenedorId}', headers=headersForSent) if contenedorId else None
 
-            ordenSqlData = responseOrdenData.json()['data'] if responseOrdenData else None
+            ordenSqlData = responseOrdenData.json(
+            )['data'] if responseOrdenData else None
 
             if not ordenSqlData:
                 continue
-            ordenMongoId = ordenSqlData.get('idMongoProductos') if ordenSqlData else None
-            responseOrdenMongoData = requests.get(f'{apiOrdenMongoId}/{ordenMongoId}', headers=headersForSent) if ordenMongoId else None
-            ordenMongoData = responseOrdenMongoData.json().get('data') if responseOrdenMongoData else None
+            ordenMongoId = ordenSqlData.get(
+                'idMongoProductos') if ordenSqlData else None
+            responseOrdenMongoData = requests.get(
+                f'{apiOrdenMongoId}/{ordenMongoId}', headers=headersForSent) if ordenMongoId else None
+            ordenMongoData = responseOrdenMongoData.json().get(
+                'data') if responseOrdenMongoData else None
             origen = ordenSqlData.get('origen') if ordenSqlData else None
-            fechaSalida = ordenMongoData.get('fechaSalida') if ordenMongoData else None
-            productosContenido = ordenMongoData.get('products') if ordenMongoData else None
+            fechaSalida = ordenMongoData.get(
+                'fechaSalida') if ordenMongoData else None
+            productosContenido = ordenMongoData.get(
+                'products') if ordenMongoData else None
 
             for productoContenido in productosContenido:
-                productoContenido['requestedQuantity'] = int(productoContenido['requestedQuantity']) if productoContenido.get('requestedQuantity') else 0
-                productoContenido['salePrice'] = float(productoContenido['salePrice']) if productoContenido.get('salePrice') else productoContenido['salePrice']
+                productoContenido['requestedQuantity'] = int(
+                    productoContenido['requestedQuantity']) if productoContenido.get('requestedQuantity') else 0
+                productoContenido['salePrice'] = float(productoContenido['salePrice']) if productoContenido.get(
+                    'salePrice') else productoContenido['salePrice']
 
                 print(productoContenido)
 
             # Creamos la lista de los camiones
-            remolque = Remolque(id_remolque=contenedorId, fecha_salida=fechaSalida, origen=origen, contenido=productosContenido, rental=True)
+            remolque = Remolque(id_remolque=contenedorId, fecha_salida=fechaSalida,
+                                origen=origen, contenido=productosContenido, rental=True)
 
             remolques.append(remolque if remolque else None)
 
         # Obtenemos productos prioritarios
-        responsePrioritarios = requests.get(apiPrioridadProductoAll, headers=headersForSent)
-        prioritariosData = responsePrioritarios.json().get('data')[0] if responsePrioritarios.status_code == 200 else None
+        responsePrioritarios = requests.get(
+            apiPrioridadProductoAll, headers=headersForSent)
+        prioritariosData = responsePrioritarios.json().get(
+            'data')[0] if responsePrioritarios.status_code == 200 else None
         productsList = prioritariosData.get('products')
 
         for productList in productsList:
